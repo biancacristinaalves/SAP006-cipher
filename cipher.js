3783c04cdb0a0f4fad266e034dae5feca43935b3
Bianca Alves <bianca.design@hotmail.com>
2021-06-15 15:06:13 -0300

    Updates

---
+++ cipher.js
@@ -0,0 +1,37 @@
+const cipher = { 
+
+  //função de codificar
+  encode: function(offset, string){
+    let code = "";                                          //"" mensagem que a pessoa vai colocar // vazia pq nao sabemos o que vão escrever
+//fazer a função de console.log para testar SEMPRE.
+
+  for(let i=0; i<=string.length; i++){                      //string.lenght é o tamanho do texto e o i++ pra somar
+    let codeMensage = string.charCodeAt (i);                // charcodeat busca no site e o i destermina o valor de letra por letra
+
+//SE o usuário digitar de A a Z maiúscula, 
+  if(codeMensage >= 65 && codeMensage <= 90){               // ele vai pegar dentro da tabela ascii a mensagem
+    let encrypt = ((codeMensage -65+ offset)%26)+65;        //vai fazer a conta para encriptar
+    code+=String.fromCharCode(encrypt);                     // e vai chamar transformando em letra denovo
+    }
+  }
+  console.log (code);
+  return code;                                             //pedir para retornar a codificação depois dele printar
+},
+  
+  //função de decodificar
+  decode: function(offset, string){
+    let decode = "";
+    
+  for(let i=0; i<=string.length; i++){
+    let decodeMensage = string.charCodeAt (i);
+
+  if(decodeMensage >= 65 && decodeMensage <= 90){
+    let decrypt = 90-((90-decodeMensage+ offset)%26);
+     decode+=String.fromCharCode(decrypt);
+      }
+    }
+    console.log (decode);
+    return decode;
+  }
+}
+export default cipher; 
---
+++ index.js
@@ -0,0 +1,28 @@
+import cipher from './cipher.js';                                                       //importando o documento cipher.js nesse documento
+
+
+let NomeComeco = prompt("Qual seu nome?");                                              //mensagem do começo, para colocar o nome
+document.getElementById("welcome").innerHTML = "Olá, " + NomeComeco + "!";
+
+const codificarBotao = document.getElementById ("codificar");                           //variavel + nome da variavel = buscar o elemento no html pelo id + id
+codificarBotao.addEventListener("click",event => {                                      //registra uma unica espera de evento // variavel + nome do evento + nome da função
+    event.preventDefault()                                                              //evento + previne de recarregar a página
+    let offset = document.getElementById("offset").value;                               //função de offset + buscar o elemento no html pelo id
+    let mensagem = document.getElementById("mensagem").value;                           //função de mensagem + buscar o elemento no html pelo id
+    let numOffset = parseInt(offset);                                                   //ele faz a inversão de texto para número
+    document.getElementById("visualizar").innerHTML = cipher.encode(numOffset,mensagem);//pegar o elemento no html pelo id e "inserir" ele lá = documento cipher.js que faz a codificação
+    console.log (codificarBotao)                                                        //pedir para imprimir na tela a variável
+});
+
+
+const decodificarBotao = document.getElementById ("decodificar");
+decodificarBotao.addEventListener("click",event => {
+    event.preventDefault()
+    let offset = document.getElementById("offset").value;
+    let mensagem = document.getElementById("mensagem").value;
+    let numOffset = parseInt(offset);
+    document.getElementById("visualizar").innerHTML = cipher.decode(numOffset,mensagem); //pegar o elemento no html pelo id e "inserir" ele lá = documento cipher.js que faz a decodificação
+    console.log (decodificarBotao)
+});
+
+console.log(cipher);
